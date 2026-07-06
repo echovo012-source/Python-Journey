9ce861aae0718a5c0c0374ca7307d0311a2dabff
echovo012-source <echovo012@gmail.com>
2026-07-06 12:35:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+x=input("Enter a number: ")
+y=input("Enter another number: ")
+z=input("choose an operation u would like to perform: ")
+if (z== "multiply"):
+    print(int(x)*int(y))
+elif(z== "add"):
+    print(int(x)+int(y))
+elif(z== "subtract"):
+    print(int(x)-int(y))
+elif(z== "divide"):
+    print(int(X)/int(Y))
+    
+
+
+
+
